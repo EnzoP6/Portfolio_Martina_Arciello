f45871ec6d703bbe46f0e96eb8b3d86348c7a374
EnzoP6 <106398215+EnzoP6@users.noreply.github.com>
2026-06-29 12:38:46 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "@/pages/HomePage";
-import CaseStudyPage from "@/pages/CaseStudyPage";
+import HomePage from "./pages/HomePage";
+import CaseStudyPage from "./pages/CaseStudyPage";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/progetto/:slug" element={<CaseStudyPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/progetto/:slug" element={<CaseStudyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
